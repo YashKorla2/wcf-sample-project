3c6de581a33c9d30c1295bc187c43fa5ee734d8c
NoReply Amazon <no-reply@amazon.com>
2025-04-16 18:15:04 +0000

    [Commit] Iteration 16 #errors = 7 <= 11.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.Designer.cs][CS0234][56,39] The type or namespace name 'ServiceProcessInstaller' does not exist in the namespace 'System.ServiceProcess' (are you missing an assembly reference?)``.

--- wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.Designer.cs
+++ wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.Designer.cs
@@ -1,59 +1,35 @@
-﻿namespace WcfServiceLibrary.WindowsServiceHost
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary.WindowsServiceHost
 {
-    partial class ProjectInstaller
+    public class Program
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary> 
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        public static void Main(string[] args)
         {
-            if (disposing && (components != null))
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseWindowsService();
+    }
+
+    public class Worker : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
             {
-                components.Dispose();
+                // Your service logic here
+                await Task.Delay(1000, stoppingToken);
             }
-            base.Dispose(disposing);
         }
-
-        #region Component Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
-            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
-            // 
-            // serviceProcessInstaller1
-            // 
-            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
-            this.serviceProcessInstaller1.Password = null;
-            this.serviceProcessInstaller1.Username = null;
-            // 
-            // serviceInstaller1
-            // 
-            this.serviceInstaller1.Description = "Wcf Service Sample";
-            this.serviceInstaller1.ServiceName = "WcfServiceSample";
-            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
-            // 
-            // ProjectInstaller
-            // 
-            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
-            this.serviceProcessInstaller1,
-            this.serviceInstaller1});
-
-        }
-
-        #endregion
-
-        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
-        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
     }
 }
