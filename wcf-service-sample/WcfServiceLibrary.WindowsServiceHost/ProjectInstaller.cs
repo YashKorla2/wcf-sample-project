f56f88290dc606eb0e1a0baa19b791db7b50afd6
NoReply Amazon <no-reply@amazon.com>
2025-04-16 18:06:22 +0000

    [Commit] Iteration 6 #errors = 13 <= 14.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs][CS0234][5,28] The type or namespace name 'Install' does not exist in the namespace 'System.Configuration' (are you missing an assembly reference?)``.

--- wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
+++ wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
@@ -1,19 +1,37 @@
-﻿using System;
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration.Install;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace WcfServiceLibrary.WindowsServiceHost
 {
-    [RunInstaller(true)]
-    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    public class ProjectInstaller : BackgroundService
     {
-        public ProjectInstaller()
+        private readonly ILogger<ProjectInstaller> _logger;
+
+        public ProjectInstaller(ILogger<ProjectInstaller> logger)
         {
-            InitializeComponent();
+            _logger = logger;
         }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Service running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ProjectInstaller>();
+                });
     }
 }
