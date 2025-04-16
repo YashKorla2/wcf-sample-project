1a484344d5cb2b695fe6c839603c86f35dc3d565
NoReply Amazon <no-reply@amazon.com>
2025-04-16 18:07:14 +0000

    Revert "[Commit] Iteration 7 #errors = 15 <= 14."
    This reverts commit 74c125608032825c50b4d5af7ba4b24f6cd20a1f.

--- wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
+++ wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
@@ -1,16 +1,15 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
-using System.Management;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WcfServiceLibrary.WindowsServiceHost
 {
     [RunInstaller(true)]
-    public partial class ProjectInstaller : Installer
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
     {
         public ProjectInstaller()
         {
