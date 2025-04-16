4a450b99f6a5227298d041a2ab9ee3eef7298e75
NoReply Amazon <no-reply@amazon.com>
2025-04-16 18:09:41 +0000

    Revert "[Commit] Iteration 10 #errors = 18 <= 14."
    This reverts commit 0a3ebcb54b6838d44156646ecc7eeb351f394d35.

--- wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
+++ wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
@@ -1,31 +1,19 @@
-using System;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
-using System.ServiceProcess;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WcfServiceLibrary.WindowsServiceHost
 {
     [RunInstaller(true)]
     public partial class ProjectInstaller : System.Configuration.Install.Installer
     {
-        private ServiceProcessInstaller serviceProcessInstaller;
-        private ServiceInstaller serviceInstaller;
-
         public ProjectInstaller()
         {
             InitializeComponent();
-
-            serviceProcessInstaller = new ServiceProcessInstaller();
-            serviceInstaller = new ServiceInstaller();
-
-            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
-            serviceInstaller.ServiceName = "WcfServiceLibraryHost";
-            serviceInstaller.DisplayName = "WCF Service Library Host";
-            serviceInstaller.Description = "Hosts the WCF Service Library";
-            serviceInstaller.StartType = ServiceStartMode.Automatic;
-
-            Installers.Add(serviceProcessInstaller);
-            Installers.Add(serviceInstaller);
         }
     }
 }
