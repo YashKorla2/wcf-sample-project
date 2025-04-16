0a3ebcb54b6838d44156646ecc7eeb351f394d35
NoReply Amazon <no-reply@amazon.com>
2025-04-16 18:09:41 +0000

    [Commit] Iteration 10 #errors = 18 <= 14.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs][CS0234][12,66] The type or namespace name 'Install' does not exist in the namespace 'System.Configuration' (are you missing an assembly reference?)``.

--- wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
+++ wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
@@ -1,19 +1,31 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using System.ComponentModel;
 using System.Configuration.Install;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ServiceProcess;
 
 namespace WcfServiceLibrary.WindowsServiceHost
 {
     [RunInstaller(true)]
     public partial class ProjectInstaller : System.Configuration.Install.Installer
     {
+        private ServiceProcessInstaller serviceProcessInstaller;
+        private ServiceInstaller serviceInstaller;
+
         public ProjectInstaller()
         {
             InitializeComponent();
+
+            serviceProcessInstaller = new ServiceProcessInstaller();
+            serviceInstaller = new ServiceInstaller();
+
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.ServiceName = "WcfServiceLibraryHost";
+            serviceInstaller.DisplayName = "WCF Service Library Host";
+            serviceInstaller.Description = "Hosts the WCF Service Library";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            Installers.Add(serviceProcessInstaller);
+            Installers.Add(serviceInstaller);
         }
     }
 }
