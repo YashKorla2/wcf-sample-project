0107d049701b4dafe9357a58ed26e3f25de483b8
NoReply Amazon <no-reply@amazon.com>
2025-04-16 18:05:30 +0000

    Revert "[Commit] Iteration 5 #errors = 13 <= 14."
    This reverts commit c65a5fd79433093f3435a3eb3cc0167d707b7289.

--- wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
+++ wcf-service-sample/WcfServiceLibrary.WindowsServiceHost/ProjectInstaller.cs
@@ -1,46 +1,19 @@
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WcfServiceLibrary.WindowsServiceHost
 {
-    public class Program
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
     {
-        public static void Main(string[] args)
+        public ProjectInstaller()
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseWindowsService()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                });
-    }
-
-    public class Worker : BackgroundService
-    {
-        private readonly ILogger<Worker> _logger;
-
-        public Worker(ILogger<Worker> logger)
-        {
-            _logger = logger;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            InitializeComponent();
         }
     }
 }
